66d385509bd27f786f584c2d25e2a51d7ae50306
Liviu Şoşu <sosuliviu@gmail.com>
2018-04-01 22:21:11 +0300

    recursion complete

---
+++ Algorithms/Algorithms/Problems/Recursion/Queens.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Recursion
+{
+    public class Queens
+    {
+        public int N;
+        public void printBoard(int[,] board)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+        public Boolean toPlaceOrNotToPlace(int[,] board, int row, int col)
+        {
+            int i, j;
+            for (i = 0; i < col; i++)
+            {
+                if (board[row, i] == 1) return false;
+            }
+            for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i, j] == 1) return false;
+            }
+            for (i = row, j = col; j >= 0 && i < N; i++, j--)
+            {
+                if (board[i, j] == 1) return false;
+            }
+            return true;
+        }
+        public Boolean theBoardSolver(int[,] board, int col)
+        {
+            if (col >= N) return true;
+            for (int i = 0; i < N; i++)
+            {
+                if (toPlaceOrNotToPlace(board, i, col))
+                {
+                    board[i, col] = 1;
+                    if (theBoardSolver(board, col + 1)) return true;
+                    // Backtracking is hella important in this one.  
+                    board[i, col] = 0;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Problems/Recursion/Recursion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Recursion
+{
+    public class Recursion
+    {
+        public int Fibbonaci(int position)
+        {
+            if (position == 0)
+            {
+                return 1;
+            }
+            if (position == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibbonaci(position - 2) + Fibbonaci(position - 1);
+            }
+        }
+
+        #region permutation of a string
+        public  void Swap(ref char a, ref char b)
+        {
+            if (a == b) return;
+
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
+
+        public  void GetPer(char[] list)
+        {
+            int x = list.Length - 1;
+            GetPer(list, 0, x);
+        }
+
+        public void GetPer(char[] list, int k, int m)
+        {
+            if (k == m)
+            {
+                Console.WriteLine(list);
+            }
+            else
+                for (int i = k; i <= m; i++)
+                {
+                    Swap(ref list[k], ref list[i]);
+                    GetPer(list, k + 1, m);
+                    Swap(ref list[k], ref list[i]);
+                }
+        }
+        #endregion
+    }
+}
