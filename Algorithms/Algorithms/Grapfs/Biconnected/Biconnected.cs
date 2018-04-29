57bdb6e950be9149cfa68eeb8dcfdfa7637bcf8f
Liviu Şoşu <sosuliviu@gmail.com>
2018-04-29 12:05:15 +0300

    biconexity and bridge added

---
+++ Algorithms/Algorithms/Grapfs/Biconnected/Biconnected.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grapfs.Biconnected
+{
+    class Biconnected
+    {
+        //https://www.geeksforgeeks.org/bridge-in-a-graph/
+        //https://msdn.microsoft.com/en-us/library/ms379574(v=vs.80).aspx
+        //https://www.hackerearth.com/practice/algorithms/graphs/biconnected-components/tutorial/
+        void CoputeConexity(){
+          //  var x = QuickGraph.Algorithms.ConnectedComponents.ConnectedComponentsAlgorithm<Object>();
+          //  x.Compute();
+        }
+    }
+}
