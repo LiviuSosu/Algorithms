e97d75983e736bf3d481cdc62cc49b24a2c84e08
Liviu Şoşu <sosuliviu@gmail.com>
2018-05-20 16:36:59 +0300

    1) Added n-queen problems 2) Solved Numbers problem from XII

--- Algorithms/Algorithms/Problems/XII/Numbers.cs
+++ Algorithms/Algorithms/Problems/XII/Numbers.cs
@@ -6,36 +6,49 @@ namespace Problems.XII
 {
     public class Numbers
     {
-        public string GetNumber(int n)
+        public List<int> GetNumber(int n)
         {
             List<int> arr = new List<int>() { 3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-            string result = "1 2";
-            string _result = "";
-            double _count = Math.Pow(2,n);
-            int count = (int)_count;
-            int sw = 1;
-            int _sw = 0;
-            //for (int i = 3; i < count; i++)
-            //{
-            //    for (int j = 0; j < sw; j++)
-            //    {
-            //        _result += (i).ToString() + " ";
-            //    }
-            //    result = _result + result;
-            //    _result = "";
+            List<int> result = new List<int>() { 1,2};
 
-            //    for (int j = 0; j < sw; j++)
-            //    {
-            //        _result += " " + (i + sw).ToString();
-            //    }
-            //    result = result + _result;
-            //    _result = "";
-            //    sw++;
-
-            //}
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n-1; i++)
             {
-                
+                double _count = Math.Pow(2, i);
+
+                var c1 = GetFirstNElements(arr, (int)_count);
+                result = ConcatenateLists(c1, result);
+
+                var c2 = GetFirstNElements(arr, (int)_count);
+                result = ConcatenateLists(result, c2);
+            }
+
+
+            return result;
+        }
+
+        private List<int> GetFirstNElements(List<int> numbers, int n)
+        {
+            List<int> result = new List<int>();
+            for (int i=0; i<n;i++)
+            {
+                result.Add(numbers[i]);
+            }
+
+            numbers.RemoveRange(0,n);
+            return result;
+        }
+
+        private List<int> ConcatenateLists(List<int> list1, List<int> list2)
+        {
+            List<int> result = new List<int>();
+            for (int i=0;i<list1.Count;i++)
+            {
+                result.Add(list1[i]);
+            }
+
+            for (int i = 0; i < list2.Count; i++)
+            {
+                result.Add(list2[i]);
             }
 
             return result;
